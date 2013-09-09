5a9a6b9ead643ebeb3588b2502afcb46cadde5db
Jan Horava <czechinthemail@gmail.com>
2013-09-08 20:26:53 -0700

    added color value out of 1, fixed hexlength bug

--- js/color-selector.js
+++ js/color-selector.js
@@ -6,54 +6,73 @@ function hexvalues(i){
     return '0123456789abcdef'.charAt(i);
 }
 
-
 function update_display(){
     // updates the display square background color
     get('display').style.background = 'rgb(' + parseInt(get('red').value) + ','
                                              + parseInt(get('green').value) + ','
-                                             + parseInt(get('blue').value) + ')'
+                                             + parseInt(get('blue').value) + ')';
+}
+
+function update_from1(color){
+    // update value of slider when 1 text input is changed
+    // validate 1 text input value isn't less than 0 or greater than 1
+    if(get(color + '-1').value < 0 || get(color + '-1').value > 1){
+        get(color + '-1').value = 0;
+    }
+
+    // if 1 text input value is a number, proceed
+    if(!isNaN(get(color + '-1').value)){
+        // update the sliders, color value out of 255, and the display
+        get(color).value = Math.round(get(color + '-1').value * 255);
+        get(color + '-255').value = Math.round(get(color + '-1').value * 255);
+        update_display();
+    }
+    // if 1 text input is not a number, don't do anything in case users are entering '.'
+}
+
+function update_from255(color){
+    // update value of slider when 255 text input is changed
+    // validate 255 text input value is a number that isn't less than 0 or greater than 255
+    if(isNaN(get(color + '-255').value)
+          || get(color + '-255').value < 0
+          || get(color + '-255').value > 255){
+        get(color + '-255').value = 0;
+    }
+
+    // if 255 text input length is zero, just use color value of 0 instead of messing with user input
+    get(color).value = get(color + '-255').value.length < 1 ? 0 : get(color + '-255').value;
+    get(color + '-1').value = get(color + '-255').value.length < 1 ? 0 : (get(color + '-255').value / 255).toFixed(4);
+    update_display();
 }
 
 function update_fromhex(){
     // update values of slider/text inputs when hex input is changed
     // hex length of 3 is valid
-    var hexlength = get('hex').length === 3;
-    get('blue-value').value = parseInt(hexlength
-                                     ? get('hex').value.substring(2, 3) + '' + get('hex').value.substring(2, 3)
+    var hexlength = get('hex').value.length === 3;
+    get('blue-255').value = parseInt(hexlength
+                                     ? get('hex').value.substring(2, 3) + get('hex').value.substring(2, 3)
                                      : get('hex').value.substring(4, 6)
-                              , 16);
-    get('green-value').value = parseInt(hexlength
-                                      ? get('hex').value.substring(1, 2) + '' + get('hex').value.substring(1, 2)
+                             , 16);
+    get('green-255').value = parseInt(hexlength
+                                      ? get('hex').value.substring(1, 2) + get('hex').value.substring(1, 2)
                                       : get('hex').value.substring(2, 4)
-                               , 16);
-    get('red-value').value = parseInt(hexlength
-                                    ? get('hex').value.substring(0, 1) + '' + get('hex').value.substring(0, 1)
+                             , 16);
+    get('red-255').value = parseInt(hexlength
+                                    ? get('hex').value.substring(0, 1) + get('hex').value.substring(0, 1)
                                     : get('hex').value.substring(0, 2)
                              , 16);
-    update_fromtext('blue');
-    update_fromtext('green');
-    update_fromtext('red');
+    update_from255('blue');
+    update_from255('green');
+    update_from255('red');
     update_display();
 }
+
 function update_fromslider(color){
-    // update values of hex and text input when slider is slided
-    get(color + '-value').value = get(color).value;
+    // update values of hex and text inputs when slider is slided
+    get(color + '-1').value = (get(color).value / 255).toFixed(4);
+    get(color + '-255').value = get(color).value;
     update_display();
     update_hex();
-}
-
-function update_fromtext(color){
-    // update value of slider when text input is changed
-    // validate text input value is a number that isn't less than 0 or greater than 255
-    if(isNaN(get(color + '-value').value)
-          || get(color + '-value').value < 0
-          || get(color + '-value').value > 255){
-        get(color + '-value').value = 0;
-    }
-
-    // if text input length is zero, just use color value of 0 instead of messing with user input
-    get(color).value = get(color + '-value').value.length < 1 ? 0 : get(color + '-value').value;
-    update_display();
 }
 
 function update_hex(){
@@ -61,8 +80,8 @@ function update_hex(){
     var red = Math.max(0, Math.min(parseInt(get('red').value, 10), 255));
     var green = Math.max(0, Math.min(parseInt(get('green').value, 10), 255));
     var blue = Math.max(0, Math.min(parseInt(get('blue').value, 10), 255));
-    get('hex').value = hexvalues((red - red % 16) / 16) + hexvalues(red % 16) + ''
-                     + hexvalues((green - green % 16) / 16) + hexvalues(green % 16) + ''
+    get('hex').value = hexvalues((red - red % 16) / 16) + hexvalues(red % 16)
+                     + hexvalues((green - green % 16) / 16) + hexvalues(green % 16)
                      + hexvalues((blue - blue % 16) / 16) + hexvalues(blue % 16);
 }
 
@@ -71,9 +90,15 @@ get('blue').oninput = function(){
     update_fromslider('blue');
 };
 
-get('blue-value').oninput = function(){
-    // event fires when user changes blue text input
-    update_fromtext('blue');
+get('blue-1').oninput = function(){
+    // event fires when user changes blue 1 text input
+    update_from1('blue');
+    update_hex();
+};
+
+get('blue-255').oninput = function(){
+    // event fires when user changes blue 255 text input
+    update_from255('blue');
     update_hex();
 };
 
@@ -82,9 +107,15 @@ get('green').oninput = function(){
     update_fromslider('green');
 };
 
-get('green-value').oninput = function(){
-    // event fires when user changes green text input
-    update_fromtext('green');
+get('green-1').oninput = function(){
+    // event fires when user changes green 1 text input
+    update_from1('green');
+    update_hex();
+};
+
+get('green-255').oninput = function(){
+    // event fires when user changes green 255 text input
+    update_from255('green');
     update_hex();
 };
 
@@ -96,8 +127,14 @@ get('red').oninput = function(){
     update_fromslider('red');
 };
 
-get('red-value').oninput = function(){
-    // event fires when user changes red text input
-    update_fromtext('red');
+get('red-1').oninput = function(){
+    // event fires when user changes red 1 text input
+    update_from1('red');
+    update_hex();
+};
+
+get('red-255').oninput = function(){
+    // event fires when user changes red 255 text input
+    update_from255('red');
     update_hex();
 };
