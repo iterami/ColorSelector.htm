11e5ec75c5d2b1f4b7e2a193a95b375f317b6266
honzi <czechinthemail@gmail.com>
2017-05-29 15:26:57 -0700

    added core_init(), repo_init(), and began global inputs

--- js/colorselector.js
+++ js/colorselector.js
@@ -197,6 +197,166 @@ function random_color(color){
     }
 }
 
+function repo_init(){
+    core_input_binds_add({
+      'keybinds': {
+        71: {
+          'todo': function(){
+              random_color('green');
+          },
+        },
+        72: {
+          'todo': function(){
+              document.getElementById('hex').value = core_random_hex();
+              update_fromhex();
+          },
+        },
+        75: {
+          'todo': function(){
+              darken_lighten(0);
+          },
+        },
+        76: {
+          'todo': function(){
+              darken_lighten(1);
+          },
+        },
+        78: {
+          'todo': function(){
+              wcag_set('background');
+          },
+        },
+        79: {
+          'todo': function(){
+              wcag_set('foreground');
+          },
+        },
+        82: {
+          'todo': function(){
+              random_color('red');
+          },
+        },
+        83: {
+          'todo': wcag_switch,
+        },
+        85: {
+          'todo': function(){
+              random_color('blue');
+          },
+        },
+      },
+    });
+
+    // Event fires when user changes hex text input.
+    document.getElementById('hex').oninput = update_fromhex;
+
+    // Event fires when user updates the hex-color color input.
+    document.getElementById('hex-color').oninput = function(){
+        document.getElementById('hex').value =
+          document.getElementById('hex-color').value.substring(
+            1,
+            7
+          );
+
+        update_fromhex();
+    };
+
+    document.getElementById('wcag-background-color').oninput = function(){
+        update_wcag(
+          'wcag-background',
+          'wcag-background-color',
+          document.getElementById('wcag-background-color').value.length
+        );
+    };
+
+    document.getElementById('wcag-background').oninput = function(){
+        update_wcag(
+          'wcag-background-color',
+          'wcag-background',
+          false
+        );
+    };
+
+    document.getElementById('wcag-foreground-color').oninput = function(){
+        update_wcag(
+          'wcag-foreground',
+          'wcag-foreground-color',
+          document.getElementById('wcag-foreground-color').value.length
+        );
+    };
+
+    document.getElementById('wcag-foreground').oninput = function(){
+        update_wcag(
+          'wcag-foreground-color',
+          'wcag-foreground',
+          false
+        );
+    };
+
+    document.getElementById('hex').value = core_random_hex();
+    update_fromhex();
+
+    update_wcag(
+      'wcag-background',
+      'wcag-background-color',
+      false
+    );
+    update_wcag(
+      'wcag-foreground',
+      'wcag-foreground-color',
+      false
+    );
+
+    var colors = [
+      'blue',
+      'green',
+      'red',
+    ];
+
+    for(var color in colors){
+        document.getElementById(colors[color] + '-random').onclick = function(){
+            var id = this.id;
+            // Event fires when color button is clicked.
+            random_color(id.substring(0, id.indexOf('-')));
+        };
+
+        document.getElementById(colors[color]).oninput = function(){
+            // Event fires when colors[color] slider is being slided.
+            update_fromslider(this.id);
+        };
+
+        document.getElementById(colors[color] + '-1').oninput = function(){
+            var id = this.id;
+            // Event fires when user changes colors[color] 1 text input.
+            update_from1(id.substring(0, id.indexOf('-')));
+            update_hex();
+        };
+
+        document.getElementById(colors[color] + '-255').oninput = function(){
+            var id = this.id;
+            // Event fires when user changes colors[color] 255 text input.
+            update_from255(id.substring(0, id.indexOf('-')));
+            update_hex();
+        };
+    }
+    document.getElementById('color-random').onclick = function(){
+        random_color();
+    };
+    document.getElementById('darken').onclick = function(){
+        darken_lighten(0);
+    };
+    document.getElementById('lighten').onclick = function(){
+        darken_lighten(1);
+    };
+    document.getElementById('wcag').onclick = wcag_switch;
+    document.getElementById('wcag-background-use').onclick = function(){
+        wcag_set('background');
+    };
+    document.getElementById('wcag-foreground-use').onclick = function(){
+        wcag_set('foreground');
+    };
+}
+
 function update_display(){
     // Set hex-color value to hex value.
     document.getElementById('hex-color').value =
@@ -397,163 +557,3 @@ function wcag_switch(){
       temp.length
     );
 }
-
-window.onload = function(e){
-    core_input_init({
-      'keybinds': {
-        71: {
-          'todo': function(){
-              random_color('green');
-          },
-        },
-        72: {
-          'todo': function(){
-              document.getElementById('hex').value = core_random_hex();
-              update_fromhex();
-          },
-        },
-        75: {
-          'todo': function(){
-              darken_lighten(0);
-          },
-        },
-        76: {
-          'todo': function(){
-              darken_lighten(1);
-          },
-        },
-        78: {
-          'todo': function(){
-              wcag_set('background');
-          },
-        },
-        79: {
-          'todo': function(){
-              wcag_set('foreground');
-          },
-        },
-        82: {
-          'todo': function(){
-              random_color('red');
-          },
-        },
-        83: {
-          'todo': wcag_switch,
-        },
-        85: {
-          'todo': function(){
-              random_color('blue');
-          },
-        },
-      },
-    });
-
-    // Event fires when user changes hex text input.
-    document.getElementById('hex').oninput = update_fromhex;
-
-    // Event fires when user updates the hex-color color input.
-    document.getElementById('hex-color').oninput = function(){
-        document.getElementById('hex').value =
-          document.getElementById('hex-color').value.substring(
-            1,
-            7
-          );
-
-        update_fromhex();
-    };
-
-    document.getElementById('wcag-background-color').oninput = function(){
-        update_wcag(
-          'wcag-background',
-          'wcag-background-color',
-          document.getElementById('wcag-background-color').value.length
-        );
-    };
-
-    document.getElementById('wcag-background').oninput = function(){
-        update_wcag(
-          'wcag-background-color',
-          'wcag-background',
-          false
-        );
-    };
-
-    document.getElementById('wcag-foreground-color').oninput = function(){
-        update_wcag(
-          'wcag-foreground',
-          'wcag-foreground-color',
-          document.getElementById('wcag-foreground-color').value.length
-        );
-    };
-
-    document.getElementById('wcag-foreground').oninput = function(){
-        update_wcag(
-          'wcag-foreground-color',
-          'wcag-foreground',
-          false
-        );
-    };
-
-    document.getElementById('hex').value = core_random_hex();
-    update_fromhex();
-
-    update_wcag(
-      'wcag-background',
-      'wcag-background-color',
-      false
-    );
-    update_wcag(
-      'wcag-foreground',
-      'wcag-foreground-color',
-      false
-    );
-
-    var colors = [
-      'blue',
-      'green',
-      'red',
-    ];
-
-    for(var color in colors){
-        document.getElementById(colors[color] + '-random').onclick = function(){
-            var id = this.id;
-            // Event fires when color button is clicked.
-            random_color(id.substring(0, id.indexOf('-')));
-        };
-
-        document.getElementById(colors[color]).oninput = function(){
-            // Event fires when colors[color] slider is being slided.
-            update_fromslider(this.id);
-        };
-
-        document.getElementById(colors[color] + '-1').oninput = function(){
-            var id = this.id;
-            // Event fires when user changes colors[color] 1 text input.
-            update_from1(id.substring(0, id.indexOf('-')));
-            update_hex();
-        };
-
-        document.getElementById(colors[color] + '-255').oninput = function(){
-            var id = this.id;
-            // Event fires when user changes colors[color] 255 text input.
-            update_from255(id.substring(0, id.indexOf('-')));
-            update_hex();
-        };
-    }
-    document.getElementById('color-random').onclick = function(){
-        random_color();
-    };
-    document.getElementById('darken').onclick = function(){
-        darken_lighten(0);
-    };
-    document.getElementById('lighten').onclick = function(){
-        darken_lighten(1);
-    };
-    document.getElementById('wcag').onclick = wcag_switch;
-    document.getElementById('wcag-background-use').onclick = function(){
-        wcag_set('background');
-    };
-    document.getElementById('wcag-foreground-use').onclick = function(){
-        wcag_set('foreground');
-    };
-};
